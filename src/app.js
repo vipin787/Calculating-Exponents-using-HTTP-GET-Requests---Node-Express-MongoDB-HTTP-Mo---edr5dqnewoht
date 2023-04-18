f78b415e73c773e28eef4249c60944e6fd491ec0
Vipin Chauhan <110002254+vipin787@users.noreply.github.com>
2023-04-18 10:28:15 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,10 +13,17 @@ const server = http.createServer((req, res) => {
       const value2 = obj.num2;
 
       // Write code here to calculate power of a number
+      if(isNaN(value1) || value1<=0 || isNaN(value2) || value2<0){
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('The operation cannot be performed')
+      }
+      else{
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(`The result is ${value1**value2}`)
+      }
       
     });
     }
 });
 
 module.exports = server;
-      
